30acc0cdaf0ffadb4fa2f0e9b09cbba0d2204f8b
Alex <anahape@ncsu.edu>
2024-11-20 10:55:18 -0500

    fixed typo

--- post-processor/flow/main.go
+++ post-processor/flow/main.go
@@ -97,7 +97,7 @@ func (agg *flowAggregator) IngestRecord(ctx *core.ExecutionContext, lineNumber i
 var scriptFlowFields = [...]string{
 	"isolate",
 	"submissionid",
-	"instanceid",
+	"scriptSHA2",
 	"visiblev8",
 	"code",
 	"url",
